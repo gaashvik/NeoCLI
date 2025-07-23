a01202d037eabd6744fe9422f4176af6fc74a958
shubh kamra <shubhkamra19@gmail.com>
2025-07-23 17:48:26 +0000

    initial mvp for the project

---
+++ tools/tools.py
@@ -0,0 +1,20 @@
+from langchain.tools import Tool
+from helpers import agent_helpers
+from models import Session
+
+session = Session.ai_shell()
+
+shell_run_tool = Tool.from_function(
+    name="shell",
+    description="Run bash shell commands. Input should be a valid shell command. and a single invocation can run a single command",
+    func=session.run,
+)
+
+explain_tool = Tool.from_function(
+    name="explain",
+    description="Explain what a shell command does.",
+    func=agent_helpers.explain_bash_command,
+)
+
+
+tools = [shell_run_tool, explain_tool]
